64e65564dd0686188d603bc2e74377ddd5e008e5
FrancoRaya <francoraya14@gmail.com>
2022-06-01 10:34:37 -0300

    entregable js coder

--- js/main.js
+++ js/main.js
@@ -3,14 +3,6 @@ let numero = parseInt(prompt("Ingresá un número para multiplicar. De lo contra
 
 //Operación
 do {
-/*     //Si el número ingresado es >= 20, no se multiplica
-    if (numero >= 20) {
-        parseInt(prompt("No se puede ingresar un número mayor o igual a 20. Ingresá un número entre 0 y 20 para multiplicar. De lo contrario, ingresá: 0")) 
-    } else {
-        
-    } */
-    
-
     //Si el número ingresado es <= 20, se multiplica
     for (let i = 0; i <= 30; i++) {
         let resultado = numero * i
@@ -20,5 +12,4 @@ do {
     //Condición de salida
     numero =prompt("Multiplicación terminada. Ingresá un número para multiplicar. De lo contrario, ingresá: 0");
 } while (numero != 0)
-
 
