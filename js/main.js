ba26ed4aba86a97ebdd758596f5e005415fd9da5
FrancoRaya <francoraya14@gmail.com>
2022-07-23 15:00:47 -0300

    prueba timer horas

--- js/main.js
+++ js/main.js
@@ -50,11 +50,13 @@ let marca = [];
     const calculateTime = runningTime => {
         const total_seconds = Math.floor(runningTime / 1000);
         const total_minutes = Math.floor(total_seconds / 60);
+        const total_hours = Math.floor(total_minutes / 60);
 
         display_seconds = (total_seconds % 60).toString().padStart(2, "0");
-        display_minutes = total_minutes.toString().padStart(2, "0");
+        display_minutes = (total_minutes % 60).toString().padStart(2, "0");
+        display_hours = total_hours.toString().padStart(2, "0");
 
-        return `${display_minutes}:${display_seconds}`;
+        return `${display_hours}:${display_minutes}:${display_seconds}`;
     };
 
     // Sumar tiempo al proyecto
